01ce9ecfe55728c0be35b2e74b625244fa55ffc4
tshelanskey <tshelanskey@bellevue.edu>
2025-03-29 10:08:53 -0500

    Test

--- Module_1/Demo.java
+++ Module_1/Demo.java
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 package Module_1;
 
 public class Demo {
-=======
-package  Module_1;
-
-public class Demo 
-{
-
->>>>>>> 7fe9faa65e3df93c1128848775ddd8abf228ad8f
     public static void main(String[] args) {
         System.out.println("Test");
+        //test comment
     }
 }
